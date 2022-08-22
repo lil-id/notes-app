97cdb1c05ca3957894ef1cf1c9664ff158c22bac
GitTest <holilhaqalim@gmail.com>
2022-08-22 11:15:08 +0800

    Can't save notes

---
+++ app/src/main/java/com/master/catatanharian/MainActivity.java
@@ -0,0 +1,163 @@
+package com.master.catatanharian;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_STORAGE = 100;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                Map<String, Object> data = (Map<String, Object>) parent.getAdapter().getItem(position);
+                intent.putExtra("filename", data.get("name").toString());
+                Toast.makeText(MainActivity.this, "You clicked " + data.get("name"), Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String, Object> data = (Map<String, Object>) parent.getAdapter().getItem(position);
+                tampilkanDialogKonfirmasiHapusCatatan(data.get("name").toString());
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_tambah:
+                Intent intent = new Intent(this, InsertActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (periksaIzinPenyimpanan()) {
+                mengambilListFilePadaFolder();
+            }
+        } else {
+            mengambilListFilePadaFolder();
+        }
+    }
+
+    public boolean periksaIzinPenyimpanan() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE);
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] granResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, granResults);
+        switch (requestCode) {
+            case REQUEST_CODE_STORAGE:
+                if (granResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mengambilListFilePadaFolder();
+                }
+                break;
+        }
+    }
+
+    void mengambilListFilePadaFolder() {
+        String path = Environment.getExternalStorageDirectory().toString() + "/kominfo";
+        File directory = new File(path);
+
+        if (directory.exists()) {
+            File[] files = directory.listFiles();
+            String[] filenames = new String[files.length];
+            String[] dateCreated = new String[files.length];
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM YYYY HH:mm:ss");
+            ArrayList<Map<String, Object>> itemDataList = new ArrayList<Map<String, Object>>();
+
+            for (int i = 0; i < files.length; i++) {
+                filenames[i] = files[i].getName();
+                Date lastModDate = new Date(files[i].lastModified());
+                Map<String, Object> listItemMap = new HashMap<>();
+                listItemMap.put("name", filenames[i]);
+                listItemMap.put("date", dateCreated[i]);
+                itemDataList.add(listItemMap);
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(this, itemDataList,
+                    android.R.layout.simple_list_item_2, new String[]{"name", "date"}, new int[]{android.R.id.text1, android.R.id.text2});
+            listView.setAdapter(simpleAdapter);
+            simpleAdapter.notifyDataSetChanged();
+        }
+    }
+
+    void tampilkanDialogKonfirmasiHapusCatatan(final String filename) {
+        new AlertDialog.Builder(this)
+                .setTitle("Hapus Catatan ini?")
+                .setMessage("Apakah Anda yakin ingin menghapus catatan" +filename+"?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        hapusFile(filename);
+                    }
+                })
+                .setNegativeButton(android.R.string.paste_as_plain_text, null).show();
+    }
+
+    void hapusFile(String filename) {
+        String path = Environment.getExternalStorageDirectory().toString() + "/kominfo";
+        File file = new File(path, filename);
+        if (file.exists()) {
+            file.delete();
+        }
+        mengambilListFilePadaFolder();
+    }
+}
